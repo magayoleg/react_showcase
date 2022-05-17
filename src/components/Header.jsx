89bfff152afbd274288c6bfd18bf4107ffd08020
Magay Oleg <59375709+magayoleg@users.noreply.github.com>
2022-05-17 21:19:51 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
   return (
     <nav className="cyan darken-2">
       <div className="nav-wrapper">
-        <a href="!#" className="brand-logo">
+        <a href="#" className="brand-logo">
           Site showcase
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
